41e05c2a79162e8ccffc8a93ca56ddd2f9df6afa
Piotr Chowaniec <chowaniec.piotr@poczta.fm>
2022-01-30 22:24:59 +0100

    No passing byte[][] array to methods in Grid

--- src/main/java/com/epam/prejap/teatrees/game/Grid.java
+++ src/main/java/com/epam/prejap/teatrees/game/Grid.java
@@ -23,40 +23,42 @@ class Grid {
     void removeCompleteLines() {
         for (int i = grid.length - 1; i > 0; i--) {
             if (isLineComplete(grid[i])) {
-                moveLinesAboveOnePositionDown(grid, i++);
-                addEmptyLineOnTop(grid);
+                moveLinesAboveOnePositionDown(i++);
+                addEmptyLineOnTop();
             }
         }
         if (isLineComplete(grid[0])) {
-            addEmptyLineOnTop(grid);
+            addEmptyLineOnTop();
         }
     }
 
-    /**
-     * If given {@code byte[] line} sum of values is equal to its length, then return true.
-     *
-     * @param line single byte array from grid.
-     * @return sum of values of given line.
-     */
     private boolean isLineComplete(byte[] line) {
         return line.length == sumLineValues(line);
     }
 
+    /**
+     * If given {@code byte[] line} sum of fields with values grater than 0 is equal to its length, then return true.
+     *
+     * @param line single byte array from grid.
+     * @return sum of fields with values grater than 0 in given line.
+     */
     private int sumLineValues(byte[] line) {
         int counter = 0;
         for (byte b : line) {
-            counter += b;
+            if (b > 0) {
+                counter++;
+            }
         }
         return counter;
     }
 
-    private void moveLinesAboveOnePositionDown(byte[][] grid, int lineCounter) {
+    private void moveLinesAboveOnePositionDown(int lineCounter) {
         for (int i = lineCounter; i > 0; i--) {
             grid[i] = grid[i - 1];
         }
     }
 
-    private void addEmptyLineOnTop(byte[][] grid) {
+    private void addEmptyLineOnTop() {
         grid[0] = new byte[grid[0].length];
     }
 
